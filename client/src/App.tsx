67efbb42b2458c2c7bfe06f2ed33d179d71bd5a8
Ram Krishna <krishnarammhd@gmail.com>
2026-02-01 10:30:51 +0530

    Make default theme ot be light

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -39,7 +39,7 @@ function Router() {
 
 function App() {
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
       <QueryClientProvider client={queryClient}>
         <TooltipProvider>
           <ScrollToTop />
